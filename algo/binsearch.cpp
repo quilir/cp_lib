a86b9c699331300aee0745ef82ba57d753f73f09
Hubert Banaszewski <hubert.banaszewski@yahoo.com>
2023-03-11 21:44:11 +0100

    Make binsearch use long longs

--- algo/binsearch.cpp
+++ algo/binsearch.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
 
 // find the first occurance of `val`
 // less, less, [[eq]], eq, eq, high, high
 template <typename T>
-int find_low_bound(vector<T> arr, int s, int e, T val) {
-    int n = e-s-1, p = e-1;
-    for (int step = 1<<(int)log2(n); step; step>>=1) {
+ll find_low_bound(vector<T> arr, ll s, ll e, T val) {
+    ll n = e-s-1, p = e-1;
+    for (ll step = 1LL<<(int)log2(n); step; step>>=1) {
         if (p-step >= s && arr[p-step]>=val) p -= step;
     }
     return p;
@@ -16,9 +17,9 @@ int find_low_bound(vector<T> arr, int s, int e, T val) {
 // find the last occurance of `val`
 // less, less, eq, eq, [[eq]], high, high
 template <typename T>
-int find_high_bound(vector<T> arr, int s, int e, T val) {
-    int n = e-s-1, p = s;
-    for (int step = 1<<(int)log2(n); step; step>>=1) {
+ll find_high_bound(vector<T> arr, ll s, ll e, T val) {
+    ll n = e-s-1, p = s;
+    for (ll step = 1LL<<(int)log2(n); step; step>>=1) {
         if (p+step < e && arr[p+step]<=val) p += step;
     }
     return p;
@@ -27,9 +28,9 @@ int find_high_bound(vector<T> arr, int s, int e, T val) {
 // get an occurance count of `val`
 // less, less, #[[eq, eq, eq]], high, high
 template <typename T>
-int get_count(vector<T> arr, int s, int e, T val) {
-    int n = e-s-1, pl = s, ph = s;
-    for (int step = 1<<(int)log2(n); step; step>>=1) {
+ll get_count(vector<T> arr, ll s, ll e, T val) {
+    ll n = e-s-1, pl = s, ph = s;
+    for (ll step = 1LL<<(int)log2(n); step; step>>=1) {
         if (pl+step < e && arr[pl+step]<val) pl += step;
         if (ph+step < e && arr[ph+step]<=val) ph += step;
     }
